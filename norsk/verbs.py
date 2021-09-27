50450b42763a9b122b8796f18d5d9543759b2151
Sam Beyer <18711431+sambeyer@users.noreply.github.com>
2021-09-27 15:20:16 +1000

    add past participle

--- norsk/verbs.py
+++ norsk/verbs.py
@@ -11,6 +11,8 @@ TOPIC = 'verbs'
 class Verb(BaseWord):
     english_past: str
     norsk_past: str
+    english_past_participle: str
+    norsk_past_participle: str
     english_subject: str = 'I'
     english_print_suffix: str = ''
 
@@ -29,8 +31,17 @@ class Verb(BaseWord):
             topic=TOPIC,
         )
 
+        past_participle_card = Card(
+            CardSide(f'{self.norsk_subject} {self.norsk_past_participle}'),
+            CardSide(
+                print_str=f'{self.english_subject} {self.english_past_participle} {self.english_print_suffix}',
+                match_str=f'{self.english_subject} {self.english_past_participle}'
+            )
+        )
+
         return [
             past_card,
+            past_participle_card
         ]
 
     @classmethod
